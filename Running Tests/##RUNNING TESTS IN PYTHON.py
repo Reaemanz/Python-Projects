8b52d2241f6d95c31b04b9a2b27bc2d4277feb70
Nsubuga Emmanuel Reagan <nsubugaemmanuel1@gmail.com>
2023-10-31 11:21:26 +0300

    Add files via upload

---
+++ Running Tests/##RUNNING TESTS IN PYTHON.py
@@ -0,0 +1,22 @@
+##RUNNING TESTS IN PYTHON
+
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+#You can run this test by saving it to a file (e.g., test_string_methods.py) 
+# and then running it with the command python -m unittest test_string_methods.py
